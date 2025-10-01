db0f488ebad1482d08c858642aa31409b816d675
Aluno SENAI <Aluno@SN-396858.edu.fiesc.com.br>
2025-10-01 17:52:31 -0300

    Ta quase

--- App.js
+++ App.js
@@ -1,63 +1,85 @@
-import { useState } from 'react';
-
-import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
-
-import { SafeAreaView } from 'react-native-safe-area-context';
-
+import React, { useState, useEffect } from 'react';
+import {SafeAreaView, StyleSheet} from 'react-native'
+import {StyleSheet,Text,View,TextInput,TouchableOpacity,SafeAreaView,ScrollView,} from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 
 export default function App() {
+  const [view, setView] = useState('lista');
+  const [recipes, setRecipes] = useState([]);
+  const [title, setTitle] = useState('');
+  const [ingredients, setIngredients] = useState('');
+
+  useEffect(()=>{
+    const loadRecipes = async ()=>{
+      try{
+        const storedRecipes = await AsyncStorage.getItem('@recipes');
+        if(storedRecipes !== null){
+          setRecipes(JSON.parse(storedRecipes));
+        }
+      } catch (e){
+        console.error("Falha ao carregar receitas.", e);
+      }
+    };
+    loadRecipes();
+  }, []);
+  // o array vazio fala para ele rodar o código apenas 1 vez quando o app abrir
+
+  const handleAddRecipe = ()=>{
+    if(!title){
+      return;
+    }
+  }
+
+  const newRecipe = {
+    id: Date.now().toString(),
+    title: title, //pega a variavel useState
+    ingredients: ingredients,
+
+    setRecipes(currentRecipes => [...currentRecipes, newRecipe]);
+  setTitle('');
+  setIngredients('');
+
+  setView('lista')
+
+  };
+
+  const handleDeleteRecipe = (id)=>{
+    setRecipes(currentRecipes => currentRecipes.filter(recipe => recipe.id !== id));
+  };
 
   return (
-    <SafeAreaView style={styles.fundo}>
-      <View>
-        <Text style={styles.h1}>
-          Chef's Choice
-        </Text>
-      </View>
-      <View style={styles.linha}/>
-      <View>
-        <TextInput placeholder='Pesquisar receitas...' style={styles.abaPesquisa}/>
-      </View>
-      <View>
-        <TouchableOpacity style={styles.botao}>
-          <Text>
-            Adicionar receita:
-          </Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.botao}>
-          <Text>
-            Deletar receita:
-          </Text>
-        </TouchableOpacity>
-      </View>
-    </SafeAreaView>
-  );
-}
+   <SafeAreaView style={StyleSheet.container}>
+    <ScrollView contentContainerStyle={styles.scrollContainer}>
+      <Text style={styles.header}>Meu livro de receitas</Text>
 
-const styles = StyleSheet.create({
-  fundo:{
-    backgroundColor: 'white',
-    flex: 1
-  },
-  h1:{
-    fontWeight: 'bold',
-    textAlign: 'center'
-},
-abaPesquisa:{
-  borderWidth: 1,
-  borderColor: 'grey',
-  borderRadius: 8,
-  width: 370,
-  height: 40,
-  alignSelf: 'center'
-},
-linha: {
-  borderBottomColor: 'gray',
-  borderBottomWidth: StyleSheet.hairlineWidth,
-  marginBottom: 10,
-},
-botao:{
-  backgroundColor: '#9de0ad',
-}
-});
+      {view === 'lista'?(
+        <View>
+          <TouchableOpacity style={styles.addButton} onPress={()=>
+            setView('formulario')}>
+              <Text style={styles.buttonText}>Adicionar nova receita</Text>
+          </TouchableOpacity>
+          {recipes.length === 0?(
+            <Text style={styles.emptyText}>Nenhuma receita cadastrada</Text>
+          ):(
+            recipes.map((item)=>(
+              <View key={item.id}style={styles.recipeItem}>
+                <View style={styles.recipeTextContainer}>
+                  <Text style={styles.recipeTitle}>{item.title}</Text>
+                  <Text style={styles.recipeIngredients}>{item.ingredients}</Text>
+                </View>
+                <TouchableOpacity
+                style={styles.deleteButton}
+                onPress={()=> handleDeleteRecipe(item.id)}>
+                <Text style={styles.buttonText}>Excluir</Text>
+                </TouchableOpacity>
+        </View>
+            ))
+          )}
+          </View>
+      ):(
+        <View style={styles.formContainer}>
+    </ScrollView>
+   </SafeAreaView>
+  );
+};
